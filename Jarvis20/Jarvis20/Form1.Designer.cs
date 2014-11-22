0649498e6c860c0cb15347870e149ba123994a07
Mike Santiago <miketheripper1@gmail.com>
2014-11-22 17:17:20 -0500

    Initial Speaking Capabilities

--- Jarvis20/Jarvis20/Form1.Designer.cs
+++ Jarvis20/Jarvis20/Form1.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.timeHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cpuHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.memHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.timeHeader,
+            this.cpuHeader,
+            this.memHeader});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(771, 331);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // timeHeader
+            // 
+            this.timeHeader.Text = "Time";
+            // 
+            // cpuHeader
+            // 
+            this.cpuHeader.Text = "CPU Load (in %)";
+            this.cpuHeader.Width = 103;
+            // 
+            // memHeader
+            // 
+            this.memHeader.Text = "Memory Left (in MB)";
+            this.memHeader.Width = 121;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(795, 493);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Jarvis Beta Build";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader timeHeader;
+        private System.Windows.Forms.ColumnHeader cpuHeader;
+        private System.Windows.Forms.ColumnHeader memHeader;
     }
 }
 
