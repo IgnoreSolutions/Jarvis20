0044277af410620e90786c1bbbe305c4f7c49aa4
Mike Santiago <miketheripper1@gmail.com>
2014-11-24 19:54:12 -0500

    started os detection class, broken

---
+++ Jarvis20/Jarvis20/DetectOperatingSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jarvis20
+{
+    public class DetectOperatingSystem
+    {
+        public enum OSFriendly { Windows95, Windows98, WindowsME, WindowsXP, WindowsVista, Windows7, Windows8, Windows81, Windows10 }
+        
+        public OSFriendly OSName()
+        {
+            float osVersion = float.Parse(Environment.OSVersion.Version.Major.ToString() + "." + Environment.OSVersion.Version.Minor.ToString());
+            switch(osVersion)
+            {
+                case(4.0f):
+                    break;
+                case(4.03f):
+                    break;
+                case(4.10f):
+                    break;
+                case(4.90):
+                    break;
+                case(5.00):
+                    break;
+                case(5.1):
+                    break;
+                case(6.0):
+                    break;
+            }
+            return null;
+        }
+    }
+}
